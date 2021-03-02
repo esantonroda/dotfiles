5d31dea2d52deef2a029d5ab932cb596357510ac
esantonroda <esantonroda@gmail.com>
2021-03-02 17:03:30 +0100

    functions modification, several tests added

--- configure.sh
+++ configure.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# install required packages
-
 DATE_BCK=$(date +%Y%m%d%H%M%S)
 OSID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 DOTFILES=$HOME/.dotfiles
 
-
 zsh_install () {
+# Software to install
 if [ -d $HOME/.oh-my-zsh ]
     then 
-        echo "Zsh installed. Installation skipped"
+        echo "Zsh already installed. Installation skipped."
     else
         echo "Zsh NOT installed. Installing..."
 # install oh-my-zsh and powerline10k theme
         bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-        bash -c "$(curl -fsSL https://gist.githubusercontent.com/romkatv/aa7a70fe656d8b655e3c324eb10f6a8b/raw/install_meslo_wsl.sh)"
 # install plugins and powerlevel10k theme
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
         git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
@@ -23,28 +20,27 @@ if [ -d $HOME/.oh-my-zsh ]
         git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "Zsh installation done."
 fi
-
+# My Dotfiles
 git clone https://github.com/esantonroda/dotfiles.git $DOTFILES
 if [ $? -eq 128 ]
     then
-        echo "fatal error on clone"
+        echo "Fatal error on dotfile clone. Pulling it..."
         cd $DOTFILES
         git pull
+        echo "Git pull done."
 fi
-
 }
 
-# fonts
-
-
 fonts_install () {
+# Fonts needed by powerline10k
 FILE=$HOME/.fonts/MesloLGS%20NF%20Regular.ttf
 
     if [ -f "$FILE" ]
         then    
-            echo "Fonts previously installed."
+            echo "Fonts already installed."
         else
-            echo "Installing fonts..."
+            echo "Installing MesloLGS Fonts..."
+# bash -c "$(curl -fsSL https://gist.githubusercontent.com/romkatv/aa7a70fe656d8b655e3c324eb10f6a8b/raw/install_meslo_wsl.sh)"
             mkdir -p $HOME/.fonts
             wget -O $HOME/.fonts/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
             wget -O $HOME/.fonts/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
@@ -65,29 +61,20 @@ rm -f $HOME/.zshrc $HOME/.p10k.zsh
 echo "Backup Done."
 }
 
-## linking to new files
-
 link_install () {
+## linking to new files
 ln -s $DOTFILES/zshrc $HOME/.zshrc 
 ln -s $DOTFILES/p10k.zsh $HOME/.p10k.zsh
 }
 
 link_install_cbld () {
+## linking to new files
 ln -s $DOTFILES/zshrc $HOME/.zshrc 
 ln -s $DOTFILES/p10k-azure.zsh $HOME/.p10k.zsh
 }
 
-# kube environment
-
-#kubectl
-## curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-## chmod +x ./kubectl
-## # Move the binary in to your PATH.
-## sudo mv ./kubectl /usr/local/bin/kubectl
-
-# kubectx+kubens
-
 kube_addons_install () {
+# kubectx+kubens
 if [ -d $HOME/.kubectx ]
     then 
         echo "Kubectx installed. Installation skipped"
@@ -108,7 +95,6 @@ fi
 }
 
 case "$OSID" in
-
 ubuntu)  echo "Installing in $OSID"
     sudo apt install zsh autojump curl git wget kubectl
     zsh_install
