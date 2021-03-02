def0a9769bee8300068c4dce83b47235d9356cdd
esantonroda <esantonroda@gmail.com>
2021-03-02 16:50:04 +0100

    functions modification, several tests added

--- configure.sh
+++ configure.sh
@@ -9,27 +9,27 @@ DOTFILES=$HOME/.dotfiles
 
 zsh_install () {
 if [ -d $HOME/.oh-my-zsh ]
-then 
-echo "Zsh installed. Installation skipped"
-else
-echo "Zsh NOT installed. Installing..."
+    then 
+        echo "Zsh installed. Installation skipped"
+    else
+        echo "Zsh NOT installed. Installing..."
 # install oh-my-zsh and powerline10k theme
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-bash -c "$(curl -fsSL https://gist.githubusercontent.com/romkatv/aa7a70fe656d8b655e3c324eb10f6a8b/raw/install_meslo_wsl.sh)"
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+        bash -c "$(curl -fsSL https://gist.githubusercontent.com/romkatv/aa7a70fe656d8b655e3c324eb10f6a8b/raw/install_meslo_wsl.sh)"
 # install plugins and powerlevel10k theme
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+        git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+        git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "Zsh installation done."
 fi
 
 git clone https://github.com/esantonroda/dotfiles.git $DOTFILES
 if [ $? -eq 128 ]
-then
-echo "fatal error on clone"
-  cd $DOTFILES
-  git pull
+    then
+        echo "fatal error on clone"
+        cd $DOTFILES
+        git pull
 fi
 
 }
@@ -42,15 +42,15 @@ FILE=$HOME/.fonts/MesloLGS%20NF%20Regular.ttf
 
     if [ -f "$FILE" ]
         then    
-        echo "Fonts previously installed."
-    else
-        echo "Installing fonts..."
-        mkdir -p $HOME/.fonts
-        wget -O $HOME/.fonts/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-        wget -O $HOME/.fonts/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-        wget -O $HOME/.fonts/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-        wget -O $HOME/.fonts/MesloLGS%20NF%20Bold%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-        echo "MesloLGS Fonts installed on $HOME/.fonts"
+            echo "Fonts previously installed."
+        else
+            echo "Installing fonts..."
+            mkdir -p $HOME/.fonts
+            wget -O $HOME/.fonts/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+            wget -O $HOME/.fonts/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+            wget -O $HOME/.fonts/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+            wget -O $HOME/.fonts/MesloLGS%20NF%20Bold%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+            echo "MesloLGS Fonts installed on $HOME/.fonts"
     fi
 }
 
@@ -89,21 +89,22 @@ ln -s $DOTFILES/p10k-azure.zsh $HOME/.p10k.zsh
 
 kube_addons_install () {
 if [ -d ~/.kubectx ]
-then 
-echo "Kubectx installed. Installation skipped"
-else
-echo "Kubectx NOT installed. Installing in local user..."
-mkdir -p ~/bin
-git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
+    then 
+        echo "Kubectx installed. Installation skipped"
+    else
+        echo "Kubectx NOT installed. Installing in local user..."
+        mkdir -p ~/bin
+        git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
 # only user install to avoid sudo
-chmod +x ~/.kubectx/kubens ~/.kubectx/kubectx
-ln -sf ~/.kubectx/kubens ~/bin/kubens
-ln -sf ~/.kubectx/kubectx ~/bin/kubectx
-mkdir -p ~/.oh-my-zsh/completions
-chmod -R 755 ~/.oh-my-zsh/completions
-ln -s ~/.kubectx/completion/kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
-ln -s ~/.kubectx/completion/kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
-echo "Kubectx installed.
+        chmod +x ~/.kubectx/kubens ~/.kubectx/kubectx
+        ln -sf ~/.kubectx/kubens ~/bin/kubens
+        ln -sf ~/.kubectx/kubectx ~/bin/kubectx
+        mkdir -p ~/.oh-my-zsh/completions
+        chmod -R 755 ~/.oh-my-zsh/completions
+        ln -s ~/.kubectx/completion/kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
+        ln -s ~/.kubectx/completion/kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
+        echo "Kubectx installed.
+fi
 }
 
 case "$OSID" in
