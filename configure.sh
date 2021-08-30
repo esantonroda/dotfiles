6bd90de7a7e8d66bd5c21f93d3e59992bcaaf5dc
Antonio Anton <74744222+esantonroda@users.noreply.github.com>
2021-08-30 14:49:05 +0200

    Updated configure.sh with powerlevel10k changes.

--- configure.sh
+++ configure.sh
@@ -17,7 +17,7 @@ if [ -d $HOME/.oh-my-zsh ]
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
         git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
         git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-        git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "Zsh installation done."
 fi
 # My Dotfiles
