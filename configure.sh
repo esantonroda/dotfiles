50771098436c69c2741008d4fb4db92f6ef2855b
Antonio Anton <74744222+esantonroda@users.noreply.github.com>
2022-10-10 17:50:08 +0200

    Update configure.sh
    added ol

--- configure.sh
+++ configure.sh
@@ -125,6 +125,15 @@ rhel)  echo "Installing in $OSID"
     link_install
     kube_addons_install
     ;;
+ol)  echo "Installing in $OSID"
+    sudo yum install zsh curl git wget
+    kubectl_install
+    zsh_install
+    fonts_install
+    zsh_backup
+    link_install
+    kube_addons_install
+    ;;
 fedora)  echo "Installing in $OSID"
     sudo yum install zsh curl git wget
     kubectl_install
